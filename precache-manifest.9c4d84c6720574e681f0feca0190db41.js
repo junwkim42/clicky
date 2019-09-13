f3fbf7d3dac3983efd083a4aa6bc857f817dd879
Scott Kim <scottkim1003@gmail.com>
2019-09-13 10:01:44 -0400

    Updates

---
+++ precache-manifest.9c4d84c6720574e681f0feca0190db41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c022e7ddbccc28596545b7c6bdf4a57",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "fde52d39fe1e191be3cf",
+    "url": "/clicky/static/css/main.d8f0424b.chunk.css"
+  },
+  {
+    "revision": "cd8a1c2521d69d3c5072",
+    "url": "/clicky/static/js/2.9ca4788e.chunk.js"
+  },
+  {
+    "revision": "fde52d39fe1e191be3cf",
+    "url": "/clicky/static/js/main.d40f6235.chunk.js"
+  },
+  {
+    "revision": "cb66523332b476d25f8c",
+    "url": "/clicky/static/js/runtime~main.352fb1e4.js"
+  }
+]);
